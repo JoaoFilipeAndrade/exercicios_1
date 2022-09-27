30c070aa4f8a6b44da3762e8dc811aeb5a329d3f
Dan Medeiros <dan.medeiros@sagewell.com>
2022-09-27 09:09:47 +0000

    §§

--- Ex1.py
+++ Ex1.py
@@ -8,3 +8,36 @@ Após o utilizador ter inserido os 5 valores apresente:
 - O maior valor inserido
 - A média das vendas
 """
+
+ilhas = ['Terceira', 'Graciosa', 'Pico', 'Faial', 'São Jorge']
+
+if __name__ == '__main__':
+    vendas = [0, 0, 0, 0, 0]
+    x = 0
+    for ilha in ilhas:
+        vendas[x] = (float(input(f'Insira as vendas para {ilha} ')))
+        x += 1
+    print(f'vendas={vendas}')
+
+    x = 0
+    while x < len(ilhas):
+        vendas.append(float(input(f'Insira as vendas para {ilhas[x]} ')))
+        x += 1
+    print(f'vendas={vendas}')
+
+
+    print(f'vendas={vendas}')
+
+"""
+    continuar = 's'
+    while continuar == 's':
+        ini = int(input('Insira o número inicial '))
+        fim = int(input('Insira o número final '))
+        primos = 0
+        for n in range(ini, fim + 1):
+            if divisores(n) == 2:
+                primos += 1
+        print(f'Entre {ini} e {fim} há {primos} de primos.')
+        continuar = input('Repetir [s | n]? ')
+    print(f'Adeus!')
+"""
