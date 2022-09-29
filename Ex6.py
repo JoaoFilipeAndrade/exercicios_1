ae381f51c3383bd87bfdaf3d9ecb2a3fe9d6f1be
Dan Medeiros <dan.medeiros@sagewell.com>
2022-09-29 11:31:09 +0000

    Funcionando v1

--- Ex6.py
+++ Ex6.py
@@ -1,9 +1,21 @@
-ilhas = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa', 'Flores', 'Corvo']
+ilhas = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa']
 tipos = ['Gasolina', 'Gasóleo']
 vendas = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
 ]
+
+"""
+vendas = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+]
+"""
+
+totais = [0, 0, 0, 0, 0]
 
 if __name__ == '__main__':
     # Obter input
@@ -33,3 +45,38 @@ if __name__ == '__main__':
         for x in range(len(vendas)):
             total += vendas[x][y]
         print(f'Total de vendas para {ilhas[y]} = {total}')
+        totais[y] = total
+
+
+    # Total de vendas
+    total = 0
+    for x in range(len(vendas)):
+        for y in range(len(vendas[x])):
+            total += vendas[x][y]
+    print(f'Total de vendas = {total}')
+
+    # Ilhas com vendas máximas
+
+    maior = totais[0]
+    menor = totais[0]
+    for x in range(1, len(totais)):
+        if totais[x] > maior:
+            maior = totais[x]
+        if totais[x] < menor:
+            menor = totais[x]
+
+    ilhas_maior = []
+    ilhas_menor = []
+
+    for x in range(len(totais)):
+        if totais[x] == maior:
+            ilhas_maior.append(ilhas[x])
+        if totais[x] == menor:
+            ilhas_menor.append(ilhas[x])
+
+    print(totais)
+    print(f'ilhas_maior = {ilhas_maior} = {maior}')
+    print(f'ilhas_menor = {ilhas_menor} = {menor}')
+
+
+
